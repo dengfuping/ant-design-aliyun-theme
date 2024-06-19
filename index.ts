73129e8783ead9526e1b20a7f07f9692cd4df2ba
dengfuping <dengfuping_private@163.com>
2024-06-19 12:12:21 +0800

    feat: Support antd v5

--- index.ts
+++ index.ts
@@ -1,138 +1,161 @@
-// All antd theme variables: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import type { ThemeConfig } from 'antd';
+
+const colorPrimary = '#0064C8';
+const colorTextHeading = 'fade(#000, 85%)';
 
 export default {
-  '@font-size-sm': '12px',
-  '@font-size-base': '12px',
-  '@primary-color': '#0064C8',
-  '@border-radius-base': '0',
-  '@border-radius-sm': '0',
-
-  '@text-color': 'fade(#000, 65%)',
-  '@text-color-secondary': 'fade(#000, 45%)',
-  '@heading-color': 'fade(#000, 85%)',
-  '@background-color-base': 'hsv(0, 0, 96%)',
-
-  '@success-color': '#1e8e3e',
-  '@error-color': '#d93026',
-  '@warning-color': '#ffc440',
-  '@info-color': '@primary-color',
-  '@danger-color': '@error-color',
-  '@processing-color': '@primary-color',
-
-  '@border-color-base': '#dedede',
-  '@border-color-split': '#ebebeb',
-
-  '@outline-width': '0',
-  '@outline-color': '#737373',
-
-  '@input-height-lg': '36px',
-  '@input-height-base': '32px',
-  '@input-height-sm': '24px',
-  '@input-hover-border-color': '#737373',
-
-  '@form-item-margin-bottom': '16px',
-
-  '@btn-default-bg': '#fafafa',
-  '@btn-default-border': '#dedede',
-  '@btn-danger-color': '#ffffff',
-  '@btn-danger-bg': '@error-color',
-  '@btn-danger-border': '@error-color',
-  '@btn-padding-horizontal-base': '12px',
-  '@btn-padding-horizontal-sm': '8px',
-  '@btn-padding-horizontal-lg': '16px',
-
-  '@switch-color': '@success-color',
-
-  '@table-header-bg': '#fafafa',
-  '@table-row-hover-bg': '#fafafa',
-  '@table-padding-vertical': '12px',
-  '@table-selected-row-bg': 'fade(@primary-color, 10)',
-  '@table-header-cell-split-color': 'transparent',
-  '@table-selection-column-width': '50px',
-
-  '@badge-color': '@error-color',
-
-  '@breadcrumb-base-color': '@text-color',
-  '@breadcrumb-last-item-color': '@text-color-secondary',
-  '@breadcrumb-link-color': '@heading-color',
-  '@breadcrumb-link-color-hover': '@primary-color',
-
-  '@slider-rail-background-color': '@background-color-base',
-  '@slider-rail-background-color-hover': '#e1e1e1',
-  '@slider-track-background-color': '@primary-color',
-  '@slider-track-background-color-hover': '@primary-color',
-  '@slider-handle-border-width': '1px',
-  '@slider-handle-color': '#dedede',
-  '@slider-handle-color-hover': '#dedede',
-  '@slider-handle-color-focus': '#dedede',
-  '@slider-handle-color-tooltip-open': '#ddd',
-  '@slider-handle-color-focus-shadow': 'transparent',
-  '@slider-handle-shadow': '1px 1px 4px 0 rgba(0,0,0,.13)',
-
-  '@alert-success-border-color': '#dff4e5',
-  '@alert-success-bg-color': '#dff4e5',
-  '@alert-info-border-color': '#e5f3ff',
-  '@alert-info-bg-color': '#e5f3ff',
-  '@alert-error-border-color': '#fcebea',
-  '@alert-error-bg-color': '#fcebea',
-  '@alert-warning-border-color': '#fff7db',
-  '@alert-warning-bg-color': '#fff7db',
-
-  '@radio-button-bg': 'transparent',
-  '@radio-button-checked-bg': 'transparent',
-  '@radio-button-active-color': 'fade(@primary-color, 15)',
-  '@radio-button-padding-horizontal': '16px',
-
-  '@progress-radius': '0',
-
-  '@tabs-card-gutter': '-1px',
-  '@tabs-card-horizontal-padding-lg': '8px 16px',
-  '@tabs-card-tab-active-border-top': '2px solid @primary-color',
-  '@tabs-hover-color': '@primary-color',
-  '@tabs-title-font-size': '14px',
-
-  '@layout-body-background': '#ffffff',
-  '@layout-sider-background': '#f5f5f5',
-
-  '@menu-bg': 'transparent',
-  '@menu-highlight-color': '@heading-color',
-  '@menu-horizontal-line-height': '36px',
-  '@menu-item-active-bg': '#dedede',
-  '@menu-item-active-border-width': '0',
-  '@menu-item-boundary-margin': '0',
-  '@menu-item-hover-bg': '#ebebeb',
-  '@menu-item-vertical-margin': '0',
-  '@menu-inline-submenu-bg': '#f5f5f5',
-
-  '@page-header-heading-title': '28px',
-  '@page-header-padding-breadcrumb': '16px',
-
-  '@tooltip-max-width': '500px',
-  '@tooltip-bg': '#ffffff',
-  '@tooltip-color': '@heading-color',
-  '@tooltip-arrow-width': '8px',
-
-  '@modal-body-padding': '0 24px',
-  '@modal-footer-border-width': '0',
-  '@modal-confirm-body-padding': '24px',
-  '@modal-confirm-title-font-size': '16px',
-  '@modal-footer-padding-vertical': '16px',
-  '@modal-footer-padding-horizontal': '24px',
-
-  '@modal-header-border-width': '0',
-
-  '@select-item-selected-bg': 'rgba(14,146,255,0.05)',
-
-  '@link-hover-color': '@primary-color',
-  '@link-hover-decoration': 'underline',
-  '@link-focus-decoration': 'underline',
-
-  '@drawer-title-font-size': '18px',
-  '@drawer-title-line-height': '27px',
-
-  '@divider-vertical-gutter': '0',
-
-  '@dropdown-selected-bg': 'rgba(14, 146, 255, 0.05)',
-
-  '@body-background': '#f2f3f5',
-};
+  token: {
+    fontSize: 12,
+    fontSizeSM: 12,
+    colorBgLayout: '#f2f3f5',
+    colorPrimary,
+    colorInfo: colorPrimary,
+    colorSuccess: '#1e8e3e',
+    colorWarning: '#ffc440',
+    colorError: '#d93026',
+    borderRadius: 0,
+    borderRadiusSM: 0,
+    colorText: 'fade(#000, 65%)',
+    colorTextSecondary: 'fade(#000, 45%)',
+    colorTextHeading,
+    colorBgBase: 'hsv(0, 0, 96%)',
+    colorBorder: '#dedede',
+    colorBorderSecondary: '#ebebeb',
+    colorLinkHover: colorPrimary,
+    linkHoverDecoration: 'underline',
+    linkFocusDecoration: 'underline',
+    controlOutlineWidth: 0,
+    controlOutline: '#737373',
+    controlHeight: 32,
+    controlHeightLG: 36,
+    controlHeightSM: 24,
+  },
+  components: {
+    Alert: {
+      colorInfoBorder: '#e5f3ff',
+      colorInfoBg: '#e5f3ff',
+      colorSuccessBorder: '#dff4e5',
+      colorSuccessBg: '#dff4e5',
+      colorWarningBorder: '#fff7db',
+      colorWarningBg: '#fff7db',
+      colorErrorBorder: '#fcebea',
+      colorErrorBg: '#fcebea',
+    },
+    Badge: {
+      colorError: '#d93026',
+    },
+    Breadcrumb: {
+      lastItemColor: 'fade(#000, 45%)',
+      linkColor: colorTextHeading,
+      linkHoverColor: colorPrimary,
+      '@breadcrumb-base-color': '@text-color',
+    },
+    Button: {
+      defaultBg: '#fafafa',
+      defaultBorderColor: '#dedede',
+      // dangerColor: '#ffffff',
+      paddingContentHorizontal: 12,
+      paddingContentHorizontalLG: 16,
+      paddingContentHorizontalSM: 8,
+      '@btn-danger-bg': '@error-color',
+      '@btn-danger-border': '@error-color',
+    },
+    Divider: {
+      verticalMarginInline: 0,
+    },
+    Drawer: {
+      '@drawer-title-font-size': '18px',
+      '@drawer-title-line-height': '27px',
+    },
+    Dropdown: {
+      controlItemBgActive: 'rgba(14, 146, 255, 0.05)',
+    },
+    Form: {
+      itemMarginBottom: 16,
+    },
+    Input: {
+      hoverBorderColor: '#737373',
+    },
+    Layout: {
+      bodyBg: '#ffffff',
+      siderBg: '#f5f5f5',
+    },
+    Menu: {
+      colorHighlight: colorTextHeading,
+      horizontalLineHeight: 36,
+      itemActiveBg: '#dedede',
+      activeBarBorderWidth: 0,
+      itemHoverBg: '#ebebeb',
+      subMenuItemBg: '#f5f5f5',
+      '@menu-bg': 'transparent',
+      '@menu-item-boundary-margin': '0',
+      '@menu-item-vertical-margin': '0',
+    },
+    Modal: {
+      '@modal-body-padding': '0 24px',
+      '@modal-footer-border-width': '0',
+      '@modal-confirm-body-padding': '24px',
+      '@modal-confirm-title-font-size': '16px',
+      '@modal-footer-padding-vertical': '16px',
+      '@modal-footer-padding-horizontal': '24px',
+      '@modal-header-border-width': '0',
+    },
+    Progress: {
+      lineBorderRadius: 0,
+    },
+    Radio: {
+      buttonBg: 'transparent',
+      buttonCheckedBg: 'transparent',
+      colorPrimaryActive: `fade(${colorPrimary}, 15)`,
+      buttonPaddingInline: 16,
+    },
+    Select: {
+      optionSelectedBg: 'rgba(14,146,255,0.05)',
+    },
+    Slider: {
+      railBg: 'hsv(0, 0, 96%)',
+      railHoverBg: '#e1e1e1',
+      trackBg: colorPrimary,
+      trackHoverBg: colorPrimary,
+      handleLineWidth: 1,
+      handleColor: '#dedede',
+      handleActiveColor: '#dedede',
+      '@slider-handle-color-hover': '#dedede',
+      '@slider-handle-color-focus': '#dedede',
+      '@slider-handle-color-tooltip-open': '#ddd',
+      '@slider-handle-color-focus-shadow': 'transparent',
+      '@slider-handle-shadow': '1px 1px 4px 0 rgba(0,0,0,.13)',
+    },
+    Switch: {
+      colorPrimary: '#1e8e3e',
+    },
+    Table: {
+      headerBg: '#fafafa',
+      rowHoverBg: '#fafafa',
+      paddingContentVertical: 12,
+      rowSelectedBg: `fade(${colorPrimary}, 10)`,
+      headerSplitColor: 'transparent',
+      selectionColumnWidth: 50,
+    },
+    Tabs: {
+      cardGutter: -1,
+      cardPaddingLG: '8px 16px',
+      titleFontSize: 14,
+      itemHoverColor: colorPrimary,
+      '@tabs-card-tab-active-border-top': '2px solid @primary-color',
+    },
+    Tooltip: {
+      colorBgSpotlight: '#ffffff',
+      colorTextLightSolid: colorTextHeading,
+      sizePopupArrow: 8,
+      '@tooltip-max-width': '500px',
+    },
+    // @deprecated, should configure it in PageContainer of pro-components
+    PageHeader: {
+      '@page-header-heading-title': '28px',
+      '@page-header-padding-breadcrumb': '16px',
+    },
+  },
+} as ThemeConfig;
